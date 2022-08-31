11b8a8d32313b5d321240da72198c1037e387dc6
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-09-01 00:37:08 +0200

    footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import "./assets/styles/reset.scss";
 import Button from "./ui/components/button";
+import Footer from "./ui/layouts/footer";
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
       <Button text="WATCH HERE!" />
+      <Footer />
     </>
   );
 }
