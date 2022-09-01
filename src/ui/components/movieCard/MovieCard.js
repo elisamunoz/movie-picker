bf463aa3cbd3cde8be2f91f29ad55c90e2156b82
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-09-01 19:59:03 +0200

    style

--- src/ui/components/movieCard/MovieCard.js
+++ src/ui/components/movieCard/MovieCard.js
@@ -1,7 +1,7 @@
 import styles from "./MovieCard.module.scss";
 
 const MovieCard = ({ onClick, alt, src }) => (
-  <div className={styles.movieCard} onClick={onClick}>
+  <div className={styles.movieCard} onClick={onClick} role="button">
     <img className={styles.image} src={src} alt={alt} />
   </div>
 );
