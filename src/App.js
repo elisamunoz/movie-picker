5d943ec3e3bebaf97180170534706b743e011ffc
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-09-01 22:14:05 +0200

    escalable modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import "assets/styles/reset.scss";
 import PageLayout from "ui/layout";
 import MovieCardList from "ui/components/movieCardList";
 import MovieCard from "ui/components/movieCard";
+import Modal from "ui/components/modal";
+import Button from "ui/components/button";
+import Pagination from "ui/components/pagination";
 import styles from "./App.module.scss";
 
 const App = () => (
   <PageLayout>
+    <Modal>
+      <h3 className={styles.title}>watch new movies for free!</h3>
+      <h5 className={styles.text}>
+        Watch any movies online for free without ads!
+      </h5>
+      <h5 className={styles.text}>Have fun watching your favourite movies!</h5>
+      <Button
+        className={styles.button}
+        text="WATCH HERE!"
+        onClick={() => console.log("you clicked me")}
+      />
+    </Modal>
+    <Pagination currentQuestion={1} totalQuestions={3} />
     <h3 className={styles.question}>Which one do you like the most?</h3>
     <MovieCardList>
       <MovieCard
--- src/ui/components/modal/Modal.js
+++ src/ui/components/modal/Modal.js
@@ -1,17 +1,11 @@
 import Button from "ui/components/button";
 import styles from "./Modal.module.scss";
 
-const Modal = ({ onClick }) => (
-  <div className={styles.modal}>
-    <div className={styles.content}>
-      <h3 className={styles.title}>watch new movies for free!</h3>
-      <h5 className={styles.text}>
-        Watch any movies online for free without ads!
-      </h5>
-      <h5 className={styles.text}>Have fun watching your favourite movies!</h5>
-      <Button className={styles.button} text="WATCH HERE!" />
+const Modal = ({ onSubmit, children, isVisible = false }) =>
+  isVisible ? (
+    <div className={styles.modal}>
+      <div className={styles.content}>{children}</div>
     </div>
-  </div>
-);
+  ) : null;
 
 export default Modal;
