5869ba0eb2b9f7203d5ba2c8e5a5a9e78cc36689
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-09-01 16:06:44 +0200

    pagination component and style

--- src/ui/layout/body/Body.js
+++ src/ui/layout/body/Body.js
@@ -1,10 +1,12 @@
 import Button from "ui/components/button";
+import Pagination from "ui/components/pagination";
 import styles from "./Body.module.scss";
 
 const Body = () => (
   <div className={styles.body}>
     <div className={styles.content}>
-      <Button text="WATCH NOW!" />
+      <Pagination currentQuestion={2} totalQuestions={3} />
+      <Button text="WATCH HERE!" />
     </div>
   </div>
 );
